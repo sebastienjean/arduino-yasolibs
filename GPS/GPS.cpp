1d6fb9e29769d8adf8b76c5965222b310c3f49eb
sebastienjean <baz.jean@gmail.com>
2012-12-03 17:32:26 +0100

    Changes to GPS lib
    - Changed license to LGPLv3 (headers updated, license file bundled)
    - Renamed the ino sketch directory according to sketch  name to avoid
    warning when using Arduino IDE

--- GPS/GPS.cpp
+++ GPS/GPS.cpp
@@ -1,17 +1,13 @@
 /*
-Copyright (C) 2012 Sebastien Jean <baz dot jean at gmail dot com>
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the version 3 GNU General Public License as
-published by the Free Software Foundation.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
+* Copyright (c) 2012 Sebastien Jean.
+* 
+* All rights reserved. This program and the accompanying materials
+* are made available under the terms of the GNU Lesser Public License v3.0
+* which accompanies this distribution, and is available at
+* http://www.gnu.org/licenses/lgpl-3.0.html
+* 
+* Contributors:
+*     Sebastien Jean - initial API and implementation
 */
 
 #include <Arduino.h> 
