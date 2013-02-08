ecc8e85ba32938357b554962db4f15f892d47f1f
sebastienjean <baz.jean@gmail.com>
2013-02-08 11:02:32 +0100

    Initial files commit for project FCOEV2

---
+++ FlyCamOneEcoV2/FCOEV2.h
@@ -0,0 +1,108 @@
+/*
+* Copyright (c) 2013 Sebastien Jean.
+* 
+* All rights reserved. This program and the accompanying materials
+* are made available under the terms of the GNU Lesser Public License v3.0
+* which accompanies this distribution, and is available at
+* http://www.gnu.org/licenses/lgpl-3.0.html
+* 
+* Contributors:
+*     Sebastien Jean - initial API and implementation
+*/
+
+#ifndef FCOEV2_h
+#define FCOEV2_h
+
+#include <inttypes.h>
+
+/**
+ * Number of periods (20ms) of IDLE signal.
+ */
+#define IDLE_PERIODS 25
+
+/**
+ * Number of periods (20ms) of ACTION signal.
+ */
+#define ACTION_PERIODS 25
+
+/**
+ * Number of periods (20ms) of CHANGE_MODE signal.
+ */
+#define CHANGE_MODE_PERIODS 200
+
+/**
+ * Width, in microseconds, of IDLE pulse.
+ */
+#define IDLE_HIGH_MICROS 1500
+
+/**
+ * PWM period, in milliseconds.
+ */
+#define PERIOD_MILLIS 20
+
+/**
+ * Width, in microseconds, of ACTION/CHANGE_MODE pulse.
+ */
+#define ACTION_OR_MODE_HIGH_MICROS 2000
+
+typedef enum {
+	MODE_VIDEO = 0,     //!< Video mode
+	MODE_PHOTO_SERIAL = 1,     //!< Serial photo mode
+	MODE_PHOTO_SINGLE = 2     //!< Single photo mode
+} FCOEV2_mode_status_enum;
+
+/**
+ * A FCOEV2 camera, attached to a given digital output.
+ */
+class FCOEV2 {
+private:
+	// per object data
+	/**
+	 * Current mode
+	 */
+	FCOEV2_mode_status_enum mode;
+
+	/**
+	 * Pin used as output for PWM signal
+	 */
+	uint8_t pwmPin;
+
+private:
+	// private methods
+
+	/**
+	 * Sending idle signal.
+	 */
+	void idle();
+
+	/**
+	 * Sending a custom signal, corresponding to a given pulse width repeated a given time.
+	 * @param pulseWidthMicros the pulse width in microseconds
+	 * @param periods the number of periods to generate
+	 */
+	void signal(uint16_t pulseWidthMicros, uint8_t periods);
+
+public:
+	// public methods
+	/**
+	 * Creates a new FCOEV2 instance, using digital IO <tt>pwmPin</tt> for PWM output.
+	 * @param pwmPin digital IO where FCOEV2 is attached
+	 */
+	FCOEV2(uint8_t pwmPin);
+
+	/**
+	 * Toggles (on/off) the action associated to current mode.
+	 */
+	void toggleAction();
+
+	/**
+	 * Switches to next operating mode.
+	 */
+	void switchToNextMode();
+
+	/**
+	 * Returns the current operating mode.
+	 */
+	FCOEV2_mode_status_enum getCurrentMode();
+};
+#endif
---
+++ FlyCamOneEcoV2/main.cpp
@@ -0,0 +1,96 @@
+/*
+* Copyright (c) 2013 Sebastien Jean.
+* 
+* All rights reserved. This program and the accompanying materials
+* are made available under the terms of the GNU Lesser Public License v3.0
+* which accompanies this distribution, and is available at
+* http://www.gnu.org/licenses/lgpl-3.0.html
+* 
+* Contributors:
+*     Sebastien Jean - initial API and implementation
+*/
+
+/**
+ * Test application for FCOEV2 library
+ *
+ * This test application supposes the FCOEV2 to be attached on pin 4 and to be on.
+ * Then it loops endlessly on:
+ * 1 - triggering a recording (when on, the first mode is VIDEO) for 5 seconds
+ * 2 - switching to PHOTO_SERIAL mode and taking photos every 4s for 30s
+ * 3 - switching to PHOTO_SINGLE mode and taking photos every second for 30s
+ *
+ * @author S.Jean
+ * @date feb. 2013
+ */
+
+#include <Arduino.h>
+#include <FCOEV2.h>
+
+
+/**
+ * FCOEV2 attached to pin 4
+ */
+FCOEV2 fcoev2(4);
+
+/**
+ * Arduino's setup function, called once at startup, after init
+ */
+void setup()
+{
+	// Nothing special to do here !
+}
+
+/**
+ * Arduino's loop function, called in loop (incredible, isn't it ?)
+ */
+void loop() // run over and over
+{
+	unsigned long startTimeMillis;
+
+	// Recording for 5 seconds
+	fcoev2.toggleAction();
+	delay(5000);
+	fcoev2.toggleAction();
+
+	delay(1000);
+
+	// Switching to Serial Photo Mode
+	fcoev2.switchToNextMode();
+	delay(1000);
+	// Taking photo every 4 seconds for 30s
+	fcoev2.toggleAction();
+	startTimeMillis = millis();
+	while (1)
+	{
+		delay(1000);
+		if ( ((millis() - startTimeMillis) /1000) > 30) break;
+	}
+	fcoev2.toggleAction();
+
+	// Switching to Serial Photo Mode
+	fcoev2.switchToNextMode();
+	// Taking photo every second for 30s
+	for (int i=0;i<30;i++)
+	{
+		delay(1000);
+		fcoev2.toggleAction();
+	}
+	// Switching to Serial Photo Mode
+	fcoev2.switchToNextMode();
+	delay(1000);
+}
+
+/**
+ * Application's main (what else to say?)
+ * @return (never)
+ */
+int main(void) {
+	init();
+
+	setup();
+
+	for (;;)
+		loop();
+
+	return 0;
+}
