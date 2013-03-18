385217792335ae7cfb5542e91b9f069153d7057f
sebastienjean <baz.jean@gmail.com>
2013-03-18 15:18:17 +0100

    Applied Gnu code style to FSK600BaudA900B1500Mod files

--- FSK600Bauds900Hz1500HzModulation/FSK600BaudTA900TB1500Mod.cpp
+++ FSK600Bauds900Hz1500HzModulation/FSK600BaudTA900TB1500Mod.cpp
@@ -1,60 +1,61 @@
 /*
-* Copyright (c) 2012 Sebastien Jean.
-* 
-* All rights reserved. This program and the accompanying materials
-* are made available under the terms of the GNU Lesser Public License v3.0
-* which accompanies this distribution, and is available at
-* http://www.gnu.org/licenses/lgpl-3.0.html
-* 
-* Contributors:
-*     Sebastien Jean - initial API and implementation
-*/
+ * Copyright (c) 2012 Sebastien Jean.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser Public License v3.0
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/lgpl-3.0.html
+ *
+ * Contributors:
+ *     Sebastien Jean - initial API and implementation
+ */
 
 #include <Arduino.h> 
 #include <FSK600BaudTA900TB1500Mod.h>
 
 FSK600BaudTA900TB1500Mod::FSK600BaudTA900TB1500Mod(uint8_t outPin)
 {
-    this->transmitPin = outPin;
-	pinMode(this->transmitPin, OUTPUT);
+  this->transmitPin = outPin;
+  pinMode(this->transmitPin, OUTPUT);
 
-	// Setting initial last state at LOW
-	this->lastState = LOW;
-	digitalWrite(this->transmitPin, LOW);
+  // Setting initial last state at LOW
+  this->lastState = LOW;
+  digitalWrite(this->transmitPin, LOW);
 }
 
-void FSK600BaudTA900TB1500Mod::generateTone(bool bit)
+void
+FSK600BaudTA900TB1500Mod::generateTone(bool bit)
 {
-	uint16_t halfPeriodUs;
-	uint8_t halfPeriodLoops;
+  uint16_t halfPeriodUs;
+  uint8_t halfPeriodLoops;
 
-	int counter = 0;
-	
-	// when HIGH, a bit is transmitted generating 1500 Hz tone during 1/600th of a second
-	if (bit)
-	{ 
-		halfPeriodUs = FSK600Baud_1500HZ_HALF_PERIOD_US;
-		halfPeriodLoops =  FSK600Baud_1500HZ_HALF_PERIOD_US_LOOPS;
-	}
-	// when LOW, a bit is transmitted generating 900 Hz tone during 1/600th of a second
-	else
-	{
-		halfPeriodUs = FSK600Baud_900HZ_HALF_PERIOD_US;
-		halfPeriodLoops =  FSK600Baud_900HZ_HALF_PERIOD_US_LOOPS;
-	}
+  int counter = 0;
 
-	for (counter=0;counter<halfPeriodLoops; counter++)
-	{
-		this->lastState = !this->lastState;
-		digitalWrite(this->transmitPin, this->lastState);
-		delayMicroseconds(halfPeriodUs);
-	}
+  // when HIGH, a bit is transmitted generating 1500 Hz tone during 1/600th of a second
+  if (bit)
+    {
+      halfPeriodUs = FSK600Baud_1500HZ_HALF_PERIOD_US;
+      halfPeriodLoops = FSK600Baud_1500HZ_HALF_PERIOD_US_LOOPS;
+    }
+  // when LOW, a bit is transmitted generating 900 Hz tone during 1/600th of a second
+  else
+    {
+      halfPeriodUs = FSK600Baud_900HZ_HALF_PERIOD_US;
+      halfPeriodLoops = FSK600Baud_900HZ_HALF_PERIOD_US_LOOPS;
+    }
+
+  for (counter = 0; counter < halfPeriodLoops; counter++)
+    {
+      this->lastState = !this->lastState;
+      digitalWrite(this->transmitPin, this->lastState);
+      delayMicroseconds(halfPeriodUs);
+    }
 }
 
-
-void FSK600BaudTA900TB1500Mod::off()
+void
+FSK600BaudTA900TB1500Mod::off()
 {
-	digitalWrite(this->transmitPin, LOW);
+  digitalWrite(this->transmitPin, LOW);
 }
 
 /**
@@ -62,33 +63,34 @@ void FSK600BaudTA900TB1500Mod::off()
  * @param c the byte to be written
  * @return
  */
-size_t FSK600BaudTA900TB1500Mod::write(uint8_t c)
+size_t
+FSK600BaudTA900TB1500Mod::write(uint8_t c)
 {
-	int loopByte;
+  int loopByte;
 
-	// save settings and turn off interrupts for a clean txmit
-	uint8_t oldSREG = SREG;
-	cli();
+  // save settings and turn off interrupts for a clean txmit
+  uint8_t oldSREG = SREG;
+  cli();
 
-	// idle state
-	generateTone(HIGH);
-	generateTone(HIGH);
-	
-	// start bit    
-	generateTone(LOW);
-		
-	// char bits, starting with LSB
-	for (loopByte=0;loopByte<8;loopByte++)
-	{
-		generateTone(c % 2);
-		c = c /2 ;
-	}
+  // idle state
+  generateTone(HIGH);
+  generateTone(HIGH);
 
-	//stop bit
-    generateTone(HIGH);
-	
-    // Restore old interrupts settings
-	SREG = oldSREG;
+  // start bit
+  generateTone(LOW);
+
+  // char bits, starting with LSB
+  for (loopByte = 0; loopByte < 8; loopByte++)
+    {
+      generateTone(c % 2);
+      c = c / 2;
+    }
+
+  //stop bit
+  generateTone(HIGH);
+
+  // Restore old interrupts settings
+  SREG = oldSREG;
 }
 
 /**
@@ -96,14 +98,31 @@ size_t FSK600BaudTA900TB1500Mod::write(uint8_t c)
  * @param bytes the array containing bytes to modulate
  * @param length the number of bytes to modulate (starting at bytes[0])
  */
-void FSK600BaudTA900TB1500Mod::modulateBytes(char *bytes, int length)
+void
+FSK600BaudTA900TB1500Mod::modulateBytes(char *bytes, int length)
 {
   for (int cpt = 0; cpt < length; cpt++)
     this->write(bytes[cpt]);
   this->off();
 }
 
-int FSK600BaudTA900TB1500Mod::read() {return -1;}
-int FSK600BaudTA900TB1500Mod::available() {return 0;}
-void FSK600BaudTA900TB1500Mod::flush() {};
-int FSK600BaudTA900TB1500Mod::peek() {return -1;}
+int
+FSK600BaudTA900TB1500Mod::read()
+{
+  return -1;
+}
+int
+FSK600BaudTA900TB1500Mod::available()
+{
+  return 0;
+}
+void
+FSK600BaudTA900TB1500Mod::flush()
+{
+}
+;
+int
+FSK600BaudTA900TB1500Mod::peek()
+{
+  return -1;
+}
--- FSK600Bauds900Hz1500HzModulation/FSK600BaudTA900TB1500Mod.h
+++ FSK600Bauds900Hz1500HzModulation/FSK600BaudTA900TB1500Mod.h
@@ -1,14 +1,14 @@
 /*
-* Copyright (c) 2012 Sebastien Jean.
-* 
-* All rights reserved. This program and the accompanying materials
-* are made available under the terms of the GNU Lesser Public License v3.0
-* which accompanies this distribution, and is available at
-* http://www.gnu.org/licenses/lgpl-3.0.html
-* 
-* Contributors:
-*     Sebastien Jean - initial API and implementation
-*/
+ * Copyright (c) 2012 Sebastien Jean.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser Public License v3.0
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/lgpl-3.0.html
+ *
+ * Contributors:
+ *     Sebastien Jean - initial API and implementation
+ */
 
 #ifndef FSKMod_h
 #define FSKMod_h
@@ -20,13 +20,11 @@
  * Duration of the 900Hz tone half-period, in microseconds
  */
 #define FSK600Baud_900HZ_HALF_PERIOD_US 550 /// 555 theorically
-
 /**
  * Duration of the 1500Hz tone half-period, in microseconds
 
  */
 #define FSK600Baud_1500HZ_HALF_PERIOD_US 326 // 333 theorically
-
 /**
  * Number of 900 Hz half-periods observed at 600 Baud
  */
@@ -43,52 +41,59 @@
 class FSK600BaudTA900TB1500Mod : public Stream
 {
 private:
-	// per object data
+  // per object data
 
-	/**
-	 * Last end of bit state (LOW/HIGH)
-	 */
-    bool lastState;
+  /**
+   * Last end of bit state (LOW/HIGH)
+   */
+  bool lastState;
 
-    /**
-     * Pin used as output
-     */
-	uint8_t transmitPin;
-	
-	// private methods
-		
+  /**
+   * Pin used as output
+   */
+  uint8_t transmitPin;
+
+  // private methods
+
 public:
-	// public methods
-	/**
-	 * Opens a FSK output stream, using a specified pin
-	 * @param outPin the pin to use for output
-	 */
-	FSK600BaudTA900TB1500Mod(uint8_t outPin);
-	
-	/**
-	 * Turns modulation off
-	 */
-	void off();
-	
-	/**
-	 * Generates tone for a single bit
-	 * @param bit the bit to transmit
-	 */
-	void generateTone(bool bit);
-	
-	/**
-	 * Modulates an array of bytes
-	 * @param bytes the array containing bytes to modulate
-	 * @param length the number of bytes to modulate (starting at bytes[0])
-	 */
-	void modulateBytes(char *bytes, int length);
+  // public methods
+  /**
+   * Opens a FSK output stream, using a specified pin
+   * @param outPin the pin to use for output
+   */
+  FSK600BaudTA900TB1500Mod(uint8_t outPin);
 
-	virtual size_t write(uint8_t byte);
-	virtual int read();
-	virtual int peek();
-	virtual int available();
-	virtual void flush();
+  /**
+   * Turns modulation off
+   */
+  void
+  off();
 
+  /**
+   * Generates tone for a single bit
+   * @param bit the bit to transmit
+   */
+  void
+  generateTone(bool bit);
+
+  /**
+   * Modulates an array of bytes
+   * @param bytes the array containing bytes to modulate
+   * @param length the number of bytes to modulate (starting at bytes[0])
+   */
+  void
+  modulateBytes(char *bytes, int length);
+
+  virtual size_t
+  write(uint8_t byte);
+  virtual int
+  read();
+  virtual int
+  peek();
+  virtual int
+  available();
+  virtual void
+  flush();
 
 };
 #endif
